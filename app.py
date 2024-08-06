c1ca8c3e664510651525c7d72f82bb98f062d5d7
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-08-06 13:59:19 +0100

    ngork url

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import subprocess
+import requests
+import webbrowser
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -15,6 +18,8 @@ def fetch_cookie(email, password):
     global driver
     print("starting the driver ...")
     options = webdriver.ChromeOptions()
+
+    options.add_experimental_option("debuggerAddress", "localhost:9250")
         
     # disable save password popup
     options.add_argument("--password-store=basic")
@@ -26,24 +31,17 @@ def fetch_cookie(email, password):
         },
     )
     
-
-    driver = webdriver.Chrome(options=options, use_subprocess=True, version_main = 126 )
+    driver = webdriver.Chrome(options=options, use_subprocess=True, version_main=126)
     driver.get("https://app.pluralsight.com/id")
-
 
     print("driver started")
     # Wait for the fields to be present and input text or click
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "Username"))).send_keys(email)
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "Password"))).send_keys(password)
-    # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "login"))).click()
     print("login clicked")
-    
 
 @app.route('/get_cookie', methods=['POST'])
 def get_cookie():
-    # email = "sami.belhadj@oddo-bhf.com"  # replace with dynamic value if needed
-    # password = "7cB3MP.6y9.Z?c?"  # replace with dynamic value if needed
-
     data = request.get_json()
     email = data.get("email")
     password = data.get("password")
@@ -56,12 +54,11 @@ def get_cookie():
     print("before fetch_cookie")
     fetch_cookie(email, password)
     print("after fetch_cookie")
-    # return a http response
-    return {"message":"Cookie fetched successfully"}
-    # keep the browser open forever
-    while True:
-        time.sleep(1)
-        pass
+    
+    response = jsonify({"message": "pluralsight opened"})
+    response.status_code = 200
+    response.headers['Custom-Header'] = 'Header-Value'
+    return response
 
 @app.route('/close', methods=['GET'])
 def close():
@@ -70,7 +67,31 @@ def close():
         driver.quit()
         driver = None
         print("Browser closed")
-        return {"message":"Browser closed"}
+        return {"message": "Browser closed"}
+    else:
+        return {"message": "No browser to close"}, 400
+
+def start_ngrok():
+    # Start ngrok tunnel
+    ngrok = subprocess.Popen(['C:/Users/weszi/Downloads/ngrok-v3-stable-windows-amd64/ngrok.exe', 'http', '5000'])
+    
+    # Allow some time for ngrok to initialize
+    time.sleep(5)
+    
+    # Fetch the ngrok public URL
+    ngrok_url = requests.get('http://localhost:4040/api/tunnels').json()['tunnels'][0]['public_url']
+    return ngrok_url
+
+
+def start():
+    # Open the Angular application with the ngrok URL as a query parameter
+    angular_url = f"http://localhost:4200/?ngrokUrl={ngrok_url}"
+    webbrowser.open(angular_url)
+    return "Angular application opened", 200
 
 if __name__ == '__main__':
+    # Start ngrok and notify the remote server
+    ngrok_url = start_ngrok()
+    response = start()
+    print(f"Ngrok URL sent to remote server ")
     app.run(debug=True)
